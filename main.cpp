bbe14b7f92c9758580365cb5074b5ebcd60b1e96
venykrid <72699147+NULL0404@users.noreply.github.com>
2022-08-18 10:53:09 -0600

    Update code for 4WD And add some personalization and comments

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ void on_center_button() {
  */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	pros::lcd::set_text(1, "A moment of silence for the deceased...");
 
 	pros::lcd::register_btn1_cb(on_center_button);
 }
@@ -74,19 +74,23 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	pros::Controller master(pros::E_CONTROLLER_MASTER); // Controller setup
+
+	// Quad motor setup
+	pros::Motor left_mtr(1,2);
+	pros::Motor right_mtr(3, 4);
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		
+		// Tank controls
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
-
 		left_mtr = left;
 		right_mtr = right;
-		pros::delay(20);
+
+		pros::delay(20); // This is required for the screen to function
 	}
 }
