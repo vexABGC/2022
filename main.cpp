e2fc680cc518d7fc2eec57e6764511eb3b9d3e9b
venykrid <72699147+NULL0404@users.noreply.github.com>
2022-12-02 15:23:49 -0700

    A lot just got done - Added working (hopefully) pneumatics -  Expansion Period #8 - Added motors for flywheels, intake, and belt - buttons for each of the above - fixed a bug where the text on the LCD was slightly too long for one line.
    yipee, now just gotta get Autonomus Period #6 and the rest of the robot
    done (Intake system  #9)

--- main.cpp
+++ main.cpp
@@ -1,129 +1,137 @@
 // --- Global variables ---
-	int LEFT_MOTOR_1_PORT = 1; 		// Left motor 1 port
-	int RIGHT_MOTOR_1_PORT = 10; 	// Right motor 1 port
-	int BAND_WHEEL_PORT = 12;		// Rubber band wheel port
-	// #define PNEUMATICS 'A' 		// PNEUMATICS port
-	int count = 0;					// Helps controller screen work
-	// bool PneumaticsState = false;// Defaults the PneumaticsState to off, as it is off when the code first runs
+	// Ports
+		int LEFT_MOTOR_1_PORT = 1; 		// Left motor 1 port
+		int RIGHT_MOTOR_1_PORT = 10; 	// Right motor 1 port
+		int BAND_WHEEL_PORT = 12;		// Rubber band wheel port
+		int FLYWHEEL_PORT_1 = 6;		// Flywheel 1 port
+		int FLYWHEEL_PORT_2 = 7;		// Flywheel 2 port
+		int BELT_MOTOR_PORT = 14;		// Motor for the belt intake
+		#define PNEUMATICS_A 'A' 		// PNEUMATICS port
+	// Other
+		int PNEUMATICS_DELAY = 200;
 
 // PROS libraries
 	#include "main.h"
 	#include "pros/misc.h"
+	#include "pros/llemu.hpp"
 
 
-void on_center_button() { // Runs when the center button of the brain's touch screen is pressed
-}
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
-	
+	/**
+		* Runs initialization code. This occurs as soon as the program is started.
+		*
+		* All other competition modes are blocked by initialize; it is recommended
+		* to keep execution time for this mode under a few seconds.
+		*/
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "A moment of silence for the deceased...");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::set_text(1, "A moment of silence");
+	pros::lcd::set_text(2, "for the deceased...");
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+void disabled() {
+	/**
+		* Runs while the robot is in the disabled state of Field Management System or
+		* the VEX Competition Switch, following either autonomous or opcontrol. When
+		* the robot is enabled, this task will exit.
+		*/
+}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
+void competition_initialize() {
+	/**
+		* Runs after initialize(), and before autonomous when connected to the Field
+		* Management System or the VEX Competition Switch. This is intended for
+		* competition-specific initialization routines, such as an autonomous selector
+		* on the LCD.
+		*
+		* This task will exit when the robot is enabled and autonomous or opcontrol
+		* starts.
+		*/
+	}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
+
 void autonomous() {
-	// Motor setup
-	pros::Motor left_mtr1(LEFT_MOTOR_1_PORT);
-	pros::Motor right_mtr1(RIGHT_MOTOR_1_PORT);
+	/**
+		* Runs the user autonomous code. This function will be started in its own task
+		* with the default priority and stack size whenever the robot is enabled via
+		* the Field Management System or the VEX Competition Switch in the autonomous
+		* mode. Alternatively, this function may be called in initialize or opcontrol
+		* for non-competition testing purposes.
+		*
+		* If the robot is disabled or communications is lost, the autonomous task
+		* will be stopped. Re-enabling the robot will restart the task, not re-start it
+		* from where it left off.
+		*/
+	// --- Motor setup ---
+		// Edit the ports using the global variables, please
+		pros::Motor left_mtr1(LEFT_MOTOR_1_PORT); 	// Left side motor
+		pros::Motor right_mtr1(RIGHT_MOTOR_1_PORT); 	// Right side motor
+		pros::Motor wheel_mtr(BAND_WHEEL_PORT);		// Rubber band wheel motor
+		pros::Motor Flywheel1(FLYWHEEL_PORT_1);		// Flywheel motor 1
+		pros::Motor Flywheel2(FLYWHEEL_PORT_2);		// Flywheel motor 2
+		pros::Motor BeltMotor(BELT_MOTOR_PORT);		// Belt intake motor
+
 
 	left_mtr1 = 100; right_mtr1 = -100;
 	pros::delay(1000);
 	left_mtr1 = 0; right_mtr1 = 0;
 	
 }
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {
 
-	// --- Controller and motor setup ---
+void opcontrol() {
+	/**
+		* Runs the operator control code. This function will be started in its own task
+		* with the default priority and stack size whenever the robot is enabled via
+		* the Field Management System or the VEX Competition Switch in the operator
+		* control mode.
+		*
+		* If no competition control is connected, this function will run immediately
+		* following initialize().
+		*
+		* If the robot is disabled or communications is lost, the
+		* operator control task will be stopped. Re-enabling the robot will restart the
+		* task, not resume it from where it left off.
+		*/
+
+	// --- Controller, pneumatics and motor setup ---
 		pros::Controller master(pros::E_CONTROLLER_MASTER); // Controller setup
 
-		// pros::ADIAnalogOut pneumatics (PNEUMATICS);	// PNEUMATICS setup
-
+		pros::ADIAnalogOut pneumaticsA (PNEUMATICS_A);	// PNEUMATICS setup
+		
 		// --- Motor setup ---
-		pros::Motor left_mtr1(LEFT_MOTOR_1_PORT); 	// Left side motor
-		pros::Motor right_mtr1(RIGHT_MOTOR_1_PORT); 	// Right side motor
-		pros::Motor wheel_mtr(BAND_WHEEL_PORT);		// Wheel motor
-
-
-		master.set_text(0, 0, "DLOW Best"); // useless
+			// Edit the ports using the global variables, please
+			pros::Motor left_mtr1(LEFT_MOTOR_1_PORT); 	// Left side motor
+			pros::Motor right_mtr1(RIGHT_MOTOR_1_PORT); 	// Right side motor
+			pros::Motor wheel_mtr(BAND_WHEEL_PORT);		// Rubber band wheel motor
+			pros::Motor Flywheel1(FLYWHEEL_PORT_1);		// Flywheel motor 1
+			pros::Motor Flywheel2(FLYWHEEL_PORT_2);		// Flywheel motor 2
+			pros::Motor BeltMotor(BELT_MOTOR_PORT);		// Belt intake motor
 
 
 
 	while (true) { // Infinite loop while the opcontrol is running to refresh controller input and output it to the motors
 
-		if (!(count % 10)) { // Run every 4th time the 20ms delay loop is ran
-			// Only print every 60ms, the controller text update rate is slow
-			}
-			count++;
-
-
 		// Read controller
-			int left = master.get_analog(ANALOG_LEFT_Y);
-			int right = master.get_analog(ANALOG_RIGHT_Y);
+			int left_stickY = master.get_analog(ANALOG_LEFT_Y);
+			int right_stickY = master.get_analog(ANALOG_RIGHT_Y);
 			bool buttonA = master.get_digital(DIGITAL_A);
 			bool buttonX = master.get_digital(DIGITAL_X);
+			bool bumperL1 = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+			bool bumperL2 = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+			bool bumperR1 = master.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+			bool bumperR2 = master.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
 
-		if (buttonX) {
-
-
-
+		if (buttonX) { // PNEUMATICS function
+			pneumaticsA.set_value(true); 					// Expand piston
+			pros::delay(PNEUMATICS_DELAY); 	// Delay to allow the piston to expand
+			pneumaticsA.set_value(false);					// Retract piston
 		}
-
 		
-
-
 		// Output to motors
-			left_mtr1 = left;
-			right_mtr1 = - right;
-			wheel_mtr = buttonA;
+			left_mtr1 = left_stickY; 					// Left and right side motors move by the sticks of their respective sides (tank controls)
+			right_mtr1 = - right_stickY;				// This motor is reversed
+			wheel_mtr = buttonA;						// The band wheel moves on button A
+			Flywheel1 = bumperR1; Flywheel2 = bumperR1; // Both flywheels activate on R1
+			BeltMotor = bumperL2 - bumperL1;			// Belt goes up when L2, down with L1
 
 
 		pros::delay(20); // This is required for the screen to function
